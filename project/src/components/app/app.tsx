3f243007993e0f07cc601de03c83cd2fbbd623a9
Aliya <weliant13@gmail.com>
2022-02-15 00:39:33 +0500

    Разбиение на основные react-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type AppScreenProps = {
+  adsCount: number;
+}
+
+function App({adsCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen adsCount={adsCount} />
+  );
 }
 
 export default App;
